69e9a51ac31c1540d54365b7f4db2d6c5176c8a6
thithihlaing23 <thiritth23@gmail.com>
2024-09-26 23:53:55 +0630

    feat: RestApi Controller

---
+++ TTHDotNetCore.RestApi/Controllers/BlogsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TTHDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlogs()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBlogs()
+        {
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult PatchBlogs()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBlogs()
+        {
+            return Ok();
+        }
+
+    }
+}
