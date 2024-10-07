ce145c84372284c4e11ac3dc863f6c22a76a25ee
thithihlaing23 <thiritth23@gmail.com>
2024-10-07 08:36:33 +0630

    feat: asp.net Core web api create DataModels and ViewModels

---
+++ TTHDotNetCore.RestApi/DataModels/BlogDataModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TTHDotNetCore.RestApi.DataModels
+{
+    public class BlogDataModel
+    {
+        public int BlogId { get; set; }
+
+        public string? BlogTitle { get; set; }
+
+        public string? blogAuthor { get; set; }
+
+        public string? BlogContent { get; set; }
+
+        public bool  DeleteFlag { get; set; }
+
+
+
+    }
+}
---
+++ TTHDotNetCore.RestApi/ViewModels/BlogViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TTHDotNetCore.RestApi.ViewModels
+{
+    public class BlogViewModel
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Author { get; set; }
+             
+        public string? Content { get; set; }
+
+        public int Deleteflag { get; set; }
+
+
+
+    }
+}
