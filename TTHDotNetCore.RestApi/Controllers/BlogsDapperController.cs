ae9b4e35a01379b26b770690837997cc08d3148d
thithihlaing23 <thiritth23@gmail.com>
2024-10-08 21:58:56 +0630

    feat: asp.net core web api dapper Read method

---
+++ TTHDotNetCore.RestApi/Controllers/BlogsDapperController.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using TTHDotNetCore.RestApi.ViewModels;
+
+namespace TTHDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsDapperController : ControllerBase
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
+        
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = @"SELECT BlogId AS Id, 
+                                        BlogTitle AS Title, 
+                                        BlogAuthor AS Author, 
+                                        BlogContent AS Content, 
+                                        DeleteFlag 
+                                FROM tbl_blog 
+                                WHERE DeleteFlag = 0;";
+                List<BlogViewModel> lst = db.Query<BlogViewModel>(query).ToList();
+                return Ok(lst);
+            }
+           
+        }
+    }
+}
