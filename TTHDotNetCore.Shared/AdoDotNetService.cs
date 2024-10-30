b797275e16d431f4ecbb814dbd936e9e1bcbec26
thithihlaing23 <thiritth23@gmail.com>
2024-10-30 21:52:49 +0630

    feat: AdoDotNetService DataTable Query  Method

---
+++ TTHDotNetCore.Shared/AdoDotNetService.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace TTHDotNetCore.Shared
+{
+    public class AdoDotNetService
+    {
+        private readonly string _connectionString;
+
+        public AdoDotNetService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DataTable Query(string query, params SqlParameterModel[] sqlParameters) { 
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(query,connection);
+            if (sqlParameters != null) {
+                foreach (var sqlParameter in sqlParameters)
+                {
+                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                }
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+
+            return dt;
+
+        }
+    }
+
+    public class SqlParameterModel
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public SqlParameterModel() { }
+
+        public SqlParameterModel(string name, string value) {
+            Name = name;
+            Value = value;
+        }
+    }
+}
