e544d21b511837f7af3d14c72274700554dbd86d
thithihlaing23 <thiritth23@gmail.com>
2024-11-06 13:35:47 +0630

    feat: Minimal Api , UseBlogEndpoint CRUD

---
+++ TTHDotNetCore.MinimalApi/Blog/BlogEndPoint.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+using TTHDotNetCore.Database.Models;
+
+namespace TTHDotNetCore.MinimalApi.Blog
+{
+    public static class BlogEndPoint
+    {
+        //public static string Test(this string i)
+        //{
+        //    return i.ToString();
+        //}
+
+        public static void UseBlogEndpoint(this IEndpointRouteBuilder app )
+        {
+            app.MapGet("/blogs", () =>
+            {
+                AppDbContext db = new AppDbContext();
+                var model = db.TblBlogs.AsNoTracking().ToList();
+                return Results.Ok(model);
+            })
+.WithName("GetBlogs")
+.WithOpenApi();
+
+
+            app.MapPost("/blog", (TblBlog blog) =>
+            {
+                AppDbContext db = new AppDbContext();
+                db.TblBlogs.Add(blog);
+                db.SaveChanges();
+                return Results.Ok(blog);
+            })
+            .WithName("CreateBlog")
+            .WithOpenApi();
+
+            app.MapPut("/blogs/{id}", (int id, TblBlog blog) =>
+            {
+                AppDbContext db = new AppDbContext();
+                var item = db.TblBlogs.FirstOrDefault(x => x.BlogId == id);
+
+                if (item is null)
+                {
+                    return Results.BadRequest("No Data Found");
+                }
+
+                item.BlogTitle = blog.BlogTitle;
+                item.BlogAuthor = blog.BlogAuthor;
+                item.BlogContent = blog.BlogContent;
+
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return Results.Ok(blog);
+            })
+            .WithName("UpdateBlog")
+            .WithOpenApi();
+
+            app.MapDelete("/blogs/{id}", (int id) =>
+            {
+                AppDbContext db = new AppDbContext();
+                var item = db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+                if (item is null)
+                {
+                    return Results.BadRequest("No Data Found");
+                }
+
+                db.Entry(item).State = EntityState.Deleted;
+                db.SaveChanges();
+                return Results.Ok(item);
+
+            })
+            .WithName("DeleteBlog")
+            .WithOpenApi();
+           
+        }
+    }
+}
---
+++ TTHDotNetCore.MinimalApi/Program.cs
@@ -0,0 +1,114 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using TTHDotNetCore.Database.Models;
+using TTHDotNetCore.MinimalApi.Blog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast")
+//.WithOpenApi();
+
+
+
+//app.MapGet("/blogs", () =>
+//{
+//    AppDbContext db = new AppDbContext();
+//    var model = db.TblBlogs.AsNoTracking().ToList();
+//    return Results.Ok(model);
+//})
+//.WithName("GetBlogs")
+//.WithOpenApi();
+
+
+//app.MapPost("/blog", (TblBlog blog) =>
+//{
+//    AppDbContext db = new AppDbContext();
+//    db.TblBlogs.Add(blog);
+//    db.SaveChanges();
+//    return Results.Ok(blog);
+//})
+//.WithName("CreateBlog")
+//.WithOpenApi();
+
+//app.MapPut("/blogs/{id}", (int id, TblBlog blog) =>
+//{
+//    AppDbContext db = new AppDbContext();
+//    var item = db.TblBlogs.FirstOrDefault(x => x.BlogId == id);
+
+//    if( item is null)
+//    {
+//        return Results.BadRequest("No Data Found");
+//    }
+
+//    item.BlogTitle = blog.BlogTitle;
+//    item.BlogAuthor = blog.BlogAuthor;
+//    item.BlogContent = blog.BlogContent;
+
+//    db.Entry(item).State = EntityState.Modified;
+//    db.SaveChanges();
+//    return Results.Ok(blog);
+//})
+//.WithName("UpdateBlog")
+//.WithOpenApi();
+
+//app.MapDelete("/blogs/{id}", (int id) =>
+//{
+//    AppDbContext db = new AppDbContext();
+//    var item = db.TblBlogs.AsNoTracking().FirstOrDefault( x => x.BlogId == id);
+//    if(item is null)
+//    {
+//        return Results.BadRequest("No Data Found");
+//    }
+
+//    db.Entry(item).State = EntityState.Deleted;
+//    db.SaveChanges();
+//    return Results.Ok(item);
+
+//})
+//.WithName("DeleteBlog")
+//.WithOpenApi();
+
+
+//BlogEndPoint.Test("i");
+//"i".Test();
+
+app.UseBlogEndpoint();
+app.Run();
+
+//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+//{
+//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//}
