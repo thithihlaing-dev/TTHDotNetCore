e99e8154cda2a4c98a7a13c157d30f810c25f018
thithihlaing23 <thiritth23@gmail.com>
2024-11-06 21:28:14 +0630

    feat: C# To Json

--- TTHDotNetCore.ConsoleApp2/Program.cs
+++ TTHDotNetCore.ConsoleApp2/Program.cs
@@ -1,10 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
 using TTHDotNetCore.Database.Models;
 
 Console.WriteLine("Hello, World!");
 
-AppDbContext db = new AppDbContext();
+//AppDbContext db = new AppDbContext();
 
-var lst = db.TblBlogs.ToList();
+//var lst = db.TblBlogs.ToList();
 
 
+var blog = new BlogModel
+{
+    Id = 1,
+    Title = "Test Title",
+    Author = "Test Author",
+    Content = "Test Content",
+};
+
+//string jsonStr = JsonConvert.SerializeObject(blog); // C# to JSON
+
+//string jsonStr = JsonConvert.SerializeObject(blog , Formatting.Indented); // C# to JSON Formt type
+
+string jsonStr = blog.ToJson();
+
+Console.WriteLine(jsonStr); 
+Console.ReadLine();
+public class BlogModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Content { get; set; }
+
+}
+
+public static class Extensions // DevCode
+{
+    public static string ToJson(this object obj)
+    {
+        string jsonStr = JsonConvert.SerializeObject(obj, Formatting.Indented);
+        return jsonStr;
+    }
+}
