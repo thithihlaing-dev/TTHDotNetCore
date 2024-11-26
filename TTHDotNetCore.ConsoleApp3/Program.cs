4e8a25dc1bb34f619dc0ca634f29ffe921a49f6d
thithihlaing23 <thiritth23@gmail.com>
2024-11-26 22:32:42 +0630

    feat: ConsoleApp3 HttpClient

---
+++ TTHDotNetCore.ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+HttpClient client = new HttpClient();
+var response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+
+if (response.IsSuccessStatusCode) 
+{
+    var jsonStr = await response.Content.ReadAsStringAsync();
+    Console.WriteLine("hello1");
+    Console.WriteLine(jsonStr);
+    Console.WriteLine("hello2");
+
+}
