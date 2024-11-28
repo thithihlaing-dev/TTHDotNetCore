6ffef814afad9fda84c3638aad36c8c2989eb992
thithihlaing23 <thiritth23@gmail.com>
2024-11-28 18:38:02 +0630

    feat: HttpClient Update, Delete

--- TTHDotNetCore.ConsoleApp3/HttpClientExample.cs
+++ TTHDotNetCore.ConsoleApp3/HttpClientExample.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TTHDotNetCore.ConsoleApp3
 {
@@ -27,8 +31,8 @@ namespace TTHDotNetCore.ConsoleApp3
 
                 Console.WriteLine(jsonStr);
             }
-        
-    
+
+
 
         }
 
@@ -52,5 +56,76 @@ namespace TTHDotNetCore.ConsoleApp3
 
         }
 
+        public async Task  Create(int userId,string title,string body)
+        {
+            PostModel requestModel = new PostModel()
+            {
+                title = title,
+                body = body,
+                userId = userId
+            };// C# object
+
+            var jsonRequest = JsonConvert.SerializeObject(requestModel);//change json code
+            var content = new StringContent(jsonRequest,Encoding.UTF8,Application.Json);//(json,language,json string) Change httpContent 
+            var response = await _client.PostAsync(_postEndPoint, content);//(endpoint , httpContent)
+            if (response.IsSuccessStatusCode) 
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+
+        }
+
+        public async Task Update(int id, string title,string body,int userId)
+        {
+            PostModel requestModel = new PostModel()
+            {
+                id = id,
+                title = title,
+                body = body,
+                userId = userId
+
+            };// C# object
+
+            var jsonRequest = JsonConvert.SerializeObject(requestModel);//change to json code
+            var content = new StringContent(jsonRequest, Encoding.UTF8, Application.Json);//(json,language,json string) Change httpContent  string
+            var response = await _client.PutAsync($"{_postEndPoint}/{id}", content); ;//(endpoint , httpContent)
+            if (response.IsSuccessStatusCode) 
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            //var response = await _client.GetAsync($"{_postEndPoint}/{id}");
+            var response = await _client.DeleteAsync($"{_postEndPoint}/{id}");
+
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+            //var responseDelete = await _client.DeleteAsync($"{_postEndPoint}/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonStr);
+            }
+
+        }
+
     }
+
+
+    
+    public class PostModel
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
 }
--- TTHDotNetCore.ConsoleApp3/Program.cs
+++ TTHDotNetCore.ConsoleApp3/Program.cs
@@ -4,8 +4,14 @@ using TTHDotNetCore.ConsoleApp3;
 Console.WriteLine("Hello, World!");
 
 
-HttpClientExample example = new HttpClientExample();
-//await example.ReadAsync();
-await example.Edit(1);
-await example.Edit(110);
+HttpClientExample httpClientExample = new HttpClientExample();
+//await httpClientExample.ReadAsync();
+//await httpClientExample.Edit(1);
+//await httpClientExample.Edit(110);
+//await httpClientExample.Create(1, "title", "body");
+
+//await httpClientExample.Update(100, "title", "body",10);
+
+await httpClientExample.Delete(1);
+
 
